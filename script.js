3fc4a419a1cbd89fb09788f0e941f4d6eb3ef0be
lucas <luharaca>
2020-11-29 02:37:27 -0500

    Implement restoring the data for the game
    1. Restored data as initial state
    2. refactored the code

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
 "use strict";
 
-const myNumber = Math.trunc(Math.random() * 20) + 1;
-let highScore = 20;
+let myNumber = generateNumber();
 
+// for debug use
 console.log(myNumber);
 
-document.querySelector("button.check").addEventListener("click", function () {
+// event listeners
+document
+  .querySelector("button.check")
+  .addEventListener("click", playCheckNumberGame);
+
+document.querySelector("button.again").addEventListener("click", restoreGame);
+
+// funcitons
+function playCheckNumberGame() {
   const guess = Number(document.querySelector("input.guess").value);
-  document.querySelector(".highscore").textContent = highScore;
+  let currentScore = Number(
+    document.querySelector("span.highscore").textContent
+  );
 
   if (!guess) {
-    document.querySelector("#alert").textContent = "⛔️  No number is entered!";
-    return;
-  }
-
-  if (guess === myNumber) {
-    document.querySelector("#alert").textContent = "🎊 🎉 Congratulations!!!";
-    document.querySelector("#result").textContent = myNumber;
-
-    changeStyleForWinner();
-    return;
-  }
-
-  if (highScore > 0) {
-    document.querySelector(".highscore").textContent = --highScore;
-    if (guess > myNumber) {
-      document.querySelector("#alert").textContent = "Too high!";
-    } else {
-      document.querySelector("#alert").textContent = "Too low!";
-    }
+    warnInvalidGuess();
+  } else if (guess === myNumber) {
+    displayWinner();
+  } else if (currentScore > 0) {
+    continueGame(guess, currentScore);
   } else {
-    document.querySelector("#alert").textContent = "Sorry, the game is over 🙁";
+    stopGame();
   }
-});
+}
 
-function changeStyleForWinner() {
+function warnInvalidGuess() {
+  document.querySelector("#alert").textContent = "⛔️  No number is entered!";
+}
+
+function displayWinner() {
+  document.querySelector("#alert").textContent = "🎊 🎉 Congratulations!!!";
+  document.querySelector("#result").textContent = myNumber;
   document.querySelector("body").style.backgroundColor = "#60b347";
   document.querySelector("#result").style.width = "30rem";
+}
+
+function continueGame(guess, currentScore) {
+  document.querySelector(".highscore").textContent = --currentScore;
+  if (guess > myNumber) {
+    document.querySelector("#alert").textContent = "The number is too high!";
+  } else {
+    document.querySelector("#alert").textContent = "The number is too low!";
+  }
+}
+
+function stopGame() {
+  document.querySelector("#alert").textContent = "Sorry, the game is over 🙁";
+}
+
+function restoreGame() {
+  myNumber = generateNumber();
+  document.querySelector("span.highscore").textContent = 20;
+  document.querySelector("#alert").textContent = "Start guessing...";
+  document.querySelector("input.guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector("#result").style.width = "15rem";
+  document.querySelector("#result").textContent = "?";
+
+  // for debug use
+  console.log(myNumber);
+}
+
+function generateNumber() {
+  return Math.trunc(Math.random() * 20) + 1;
 }
